d11da8df25e22660d3081908c60f50de84180a74
Nicholas Marques <devnicknish@gmail.com>
2023-06-01 09:26:49 -0300

    style: update mobile header style

--- src/components/moleculars/Header/styles.ts
+++ src/components/moleculars/Header/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {stylizedDisplayLg} from "styles/typography/stylized";
+import {stylizedDisplayLg, stylizedDisplaySm} from "styles/typography/stylized";
 
 export const Container = styled.div`
   margin-top: ${({ theme }) => theme.spacing(16)};
@@ -14,4 +14,8 @@ export const Title = styled.h6`
   ${stylizedDisplayLg};
   margin-top: ${({ theme }) => theme.spacing(16)};
   color: ${({ theme }) => theme.colors.neutral[800]};
+  
+  @media (max-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    ${stylizedDisplaySm}
+  }
 `;
--- src/pages/contributions/ContributionPage/styles.ts
+++ src/pages/contributions/ContributionPage/styles.ts
@@ -3,9 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
   padding-left: 228px;
   padding-right: 228px;
-`;
-
-export const Title = styled.h6`
-  margin: 8px 0 24px 0;
-  color: ${({ theme }) => theme.colors.darkGray};
+  
+    @media (max-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    padding-left: 16px;
+    padding-right: 16px;
+    }
 `;
