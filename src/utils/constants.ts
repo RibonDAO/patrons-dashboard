ccac9d9db651e5edfd8eb78942a51bd4c0a3fd29
Nicholas Marques <devnicknish@gmail.com>
2023-06-05 11:28:39 -0300

    fix: lint

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,4 @@
-export const RIBON_API =
-  "https://dapp-dev-api.ribon.io/";
+export const RIBON_API = "https://dapp-dev-api.ribon.io/";
 
 export const THE_GRAPH_API =
   "https://api.thegraph.com/subgraphs/name/ribondao/subgraphribon";
