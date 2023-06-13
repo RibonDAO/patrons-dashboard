91908f26da5a272f84f0da7bdace424c6a9dbe7c
Nicholas Marques <devnicknish@gmail.com>
2023-06-13 11:09:33 -0300

    test: remove unused gift boost section specs

--- src/pages/contributions/ContributionPage/GiftBoostSection/index.test.tsx
+++ src/pages/contributions/ContributionPage/GiftBoostSection/index.test.tsx
@@ -1,11 +1,8 @@
 import { renderComponent } from "config/testUtils/renders";
 import GiftBoostSection from "pages/contributions/ContributionPage/GiftBoostSection/index";
-import { expectTextToBeInTheDocument } from "config/testUtils/expects";
 
 describe("GiftBoostSection", () => {
   it("renders without error", () => {
     renderComponent(<GiftBoostSection />);
-
-    expectTextToBeInTheDocument("info");
   });
 });
