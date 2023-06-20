81f9d7be0c97023fd9f7965ab30979e376f56b7b
mendesmcmg <claraplanejamento@gmail.com>
2023-06-20 15:51:23 -0300

    refactor: changes icon so it can have a circle

--- src/components/atomics/Icon/styles.ts
+++ src/components/atomics/Icon/styles.ts
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 
-export const Icon = styled.span<{ color?: string; size?: string }>`
+export const Icon = styled.span<{ color?: string; size?: string, withCircle?:boolean }>`
   font-size: ${(props) => props.size || props.theme.icons.xs};
   color: ${(props) => props.color || props.theme.colors.neutral[500]};
+
+  ${(props) => props.withCircle && `
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    background-color: ${props.theme.colors.neutral10};
+    padding: 0.20rem;
+  `}
 `;
