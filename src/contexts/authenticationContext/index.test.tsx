f22a10d9da5bd0b3f47a3eb4f75c7521374a6624
Nicholas Marques <devnicknish@gmail.com>
2023-06-21 15:13:21 -0300

    test: update authenticationContext tests

--- src/contexts/authenticationContext/index.test.tsx
+++ src/contexts/authenticationContext/index.test.tsx
@@ -1,4 +1,8 @@
 import { renderComponent, renderHook, waitForPromises } from "config/testUtils";
+import { REFRESH_TOKEN_KEY, TOKEN_KEY } from "utils/constants";
+import { act } from "@testing-library/react";
+import authApi from "services/api/authApi";
+import { expectCookieToHaveBeenSet } from "config/testUtils/expects";
 import { useAuthentication } from ".";
 
 function AuthenticationTestPage() {
@@ -13,16 +17,36 @@ describe("useAuthentication", () => {
     renderComponent(<AuthenticationTestPage />);
     await waitForPromises();
   });
-  describe("isAuthorized", () => {
+
+  describe("signInByAuthToken", () => {
     beforeEach(async () => {
       const { hook } = renderHook(() => useAuthentication());
       current = hook.result.current;
       await waitForPromises();
     });
-    it("should return true when email domain is @ribon.io", async () => {
-      const allowed = current.accessToken;
-      await waitForPromises();
-      expect(allowed).toBe(true);
+
+    describe("when the api returns ok", () => {
+      beforeEach(() => {
+        jest.spyOn(authApi, "postAuthorizeFromAuthToken").mockResolvedValue({
+          status: 200,
+          statusText: "ok",
+          config: {},
+          data: {},
+          headers: {
+            "access-token": "token",
+            "refresh-token": "refresh-token",
+          },
+        });
+      });
+
+      it("sets the tokens on cookies", async () => {
+        await act(async () => {
+          await current.signInByAuthToken({ authToken: "authToken", id: "id" });
+        });
+
+        expectCookieToHaveBeenSet(TOKEN_KEY, "token");
+        expectCookieToHaveBeenSet(REFRESH_TOKEN_KEY, "refresh-token");
+      });
     });
   });
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,16 @@
 import "@testing-library/jest-dom";
 import "jest-canvas-mock";
 
+export const mockCookieSet = jest.fn();
+export const mockCookieGet = jest.fn();
 jest.mock("react-chartjs-2", () => ({
   Pie: () => null,
+}));
+
+jest.mock("js-cookie", () => ({
+  __esModule: true,
+  default: {
+    set: mockCookieSet,
+    get: mockCookieGet,
+  },
 }));
