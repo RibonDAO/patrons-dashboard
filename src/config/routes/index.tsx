09f586ed0754ee3531226b4f22da70db52922efb
Nicholas Marques <devnicknish@gmail.com>
2023-06-22 14:04:33 -0300

    refactor: update contribution route to be a private route

--- src/config/routes/index.tsx
+++ src/config/routes/index.tsx
@@ -2,13 +2,39 @@ import HomePage from "pages/HomePage";
 import { Routes, Route } from "react-router-dom";
 import ContributionPage from "pages/contributions/ContributionPage";
 import SignInPage from "pages/auth/SignInPage";
+import PrivateRoute from "config/routes/privateRoute";
+import { Suspense } from "react";
 
 function RoutesComponent(): JSX.Element {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/contributions" element={<ContributionPage />} />
-      <Route path="/auth" element={<SignInPage />} />
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={<div />}>
+            <HomePage />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/contributions"
+        element={
+          <PrivateRoute>
+            <Suspense fallback={<div />}>
+              <ContributionPage />
+            </Suspense>
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/auth"
+        element={
+          <Suspense fallback={<div />}>
+            <SignInPage />
+          </Suspense>
+        }
+      />
     </Routes>
   );
 }
--- src/config/routes/privateRoute.tsx
+++ src/config/routes/privateRoute.tsx
@@ -1,7 +1,9 @@
+import { getCookiesItem } from "lib/cookies";
 import { Navigate } from "react-router-dom";
+import { TOKEN_KEY } from "utils/constants";
 
 function PrivateRoute({ children }: any) {
-  const accessToken = localStorage.getItem("token");
+  const accessToken = getCookiesItem(TOKEN_KEY);
 
   return accessToken ? children : <Navigate to="/" />;
 }
